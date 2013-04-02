77e45b867c552f2036678a5287cafa0c1b11f3ea
Anton Andriyevskyy <x.meglio@gmail.com>
2013-04-02 20:22:11 +0300

    Create meta.js

---
+++ meta.js
@@ -0,0 +1,26 @@
+{
+  "new_version": "0.4",
+  "url": "http://www.xlr8mediainc.com/",
+  "package": "http://www.xlr8mediainc.com/woocommerce-payment-gateway-multiplier-0.4.zip",
+  "info": {
+    "name": "XLR8: WooCommerce Payment Gateway Multiplier",
+    "author": "<a href='http://www.xlr8mediainc.com/'>XLR8 Media</a>",
+    "author_profile": "http://www.xlr8mediainc.com/",
+    "requires": "3.5",
+    "tested": "3.5.1",
+    "rating": 98,
+    "num_ratings": 680,
+    "downloaded": 1950,
+    "last_upated": "2013-04-02",
+    "added": "2013-04-01",
+    "homepage": "http://www.xlr8mediainc.com/woocommerce-payment-gateway-multiplier",
+    "tags": {"wordpress":"wordpress","woocommerce":"woocommerce"},
+    "sections":{
+      "description": "XLR8: WooCommerce Payment Gateway Multiplier",
+      "installation": "As simple as 2 steps: extract in plugins folder, activate in administration panel",
+      "screenshots":"",
+      "changelog":"",
+      "faq":""
+    }
+  }
+}
